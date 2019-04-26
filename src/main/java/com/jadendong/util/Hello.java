183c2d2c4d28e88ac1529779e8a75efbf9372c26
董健 <dongj@bbieat.com>
2019-04-26 09:15:18 +0800

    Hello

---
+++ src/main/java/com/jadendong/util/Hello.java
@@ -0,0 +1,11 @@
+package com.jadendong.util;
+
+/**
+ * @author : jaden dong
+ * @date : 2019/4/26 0026
+ */
+public class Hello {
+    public static String hello() {
+        return "Hello";
+    }
+}
